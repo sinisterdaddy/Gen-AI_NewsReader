0a2eba0e0b8910f7d44c1be9e27b4e1c1fb61287
Radha Krishna <krishna.garg010604@gmail.com>
2024-03-06 22:05:06 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,8 +1,1 @@
 from wsgi import app
-
-from http import HTTPStatus
-
-def handler(request, response):
-    # Your code here
-    response.status_code = HTTPStatus.OK
-    response.text = "Hello, Vercel!"
